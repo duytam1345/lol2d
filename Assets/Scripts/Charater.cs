a22730641b3eb0901e19007730e2ab28187ffa12
Duy Tam <leduytam123kgz@gmail.com>
2020-08-09 18:30:46 +0700

    init

---
+++ Assets/Scripts/Charater.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Charater : MonoBehaviour
+{
+    public float speed;
+
+    void Update()
+    {
+        Mananger.instance.m_Input();
+    }
+}
---
+++ Assets/Scripts/Mananger.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mananger : MonoBehaviour
+{
+    public static Mananger instance;
+
+    public 
+}
