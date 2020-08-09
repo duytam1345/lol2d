31bd79465aecbc044c8d5e162e9bef8be2d412fc
Duy Tam <leduytam123kgz@gmail.com>
2020-08-09 20:06:02 +0700

    them input

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public static bool m_GetMouseButtonDownLeft
+    {
+        get
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+    public static bool m_GetMouseButtonDownRight
+    {
+        get
+        {
+            return Input.GetMouseButtonDown(1);
+        }
+    }
+
+    public static Vector2 m_mousePosition
+    {
+        get
+        {
+            return Input.mousePosition;
+        }
+    }
+
+    public static bool m_KeyDownQ
+    {
+        get
+        {
+            return Input.GetKeyDown(KeyCode.Q);
+        }
+    }
+
+    public static bool m_KeyDownW
+    {
+        get
+        {
+            return Input.GetKeyDown(KeyCode.Q);
+        }
+    }
+
+    public static bool m_KeyDownE
+    {
+        get
+        {
+            return Input.GetKeyDown(KeyCode.Q);
+        }
+    }
+
+    public static bool m_KeyDownR
+    {
+        get
+        {
+            return Input.GetKeyDown(KeyCode.Q);
+        }
+    }
+
+    public static bool m_KeyDownP
+    {
+        get
+        {
+            return Input.GetKeyDown(KeyCode.Q);
+        }
+    }
+
+    public static bool SwiftCamMode
+    {
+        get
+        {
+            return Input.GetKeyDown(KeyCode.Y);
+        }
+    }
+}
