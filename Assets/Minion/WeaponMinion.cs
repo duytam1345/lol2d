f68f748ddcc3a6e5b67174a9e0bc55e2f5a31013
thanhle <thanhle@THANHLE-PC>
2020-08-09 21:33:40 +0700

    add minion

---
+++ Assets/Minion/WeaponMinion.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponMinion : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 1f;
+    Transform target;
+    public void SetTarget(Transform target)
+    {
+        this.target = target;
+    }
+    void Update()
+    {
+        if (target == null)
+        {
+            return;
+        }
+        var d = Vector3.Distance(target.position, transform.position);
+        if (d < 0.1f)
+        {
+            GameObject.Destroy(gameObject);
+            return;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    void Awake()
+    {
+        Minion.OnSpaw += OnMinionSpawn;
+    }
+    void OnMinionSpawn()
+    {
+
+    }
+    void Update()
+    {
+
+    }
+}
